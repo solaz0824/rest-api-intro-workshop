daad8a2ac15ee5fc65c23d2fc2894cd17352aae6
Jcarri14 <j.carrion@students.salle.url.edu>
2021-07-19 12:56:59 +0200

    feat: add models

---
+++ src/controllers/index.js
@@ -0,0 +1,7 @@
+const movieController = require("./movie-controller");
+const userController = require("./user-controller");
+
+module.exports = {
+  userController: userController,
+  movieController: movieController,
+};
---
+++ src/models/index.js
@@ -0,0 +1,7 @@
+const UserModel = require("./user-model");
+const MovieModel = require("./movie-model");
+
+module.exports = {
+  User: UserModel,
+  Movie: MovieModel,
+};
